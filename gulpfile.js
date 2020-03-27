eb9fbae7ede788d1783a4fe3bb3f3087c5f8e5a6
Mher-777 <mherjan2003@mail.ru>
2020-03-27 22:12:22 +0400

    gulp start

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,7 @@ let gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    cssmin = require('gulp-cssmin'),
-    imagemin = require('gulp-imagemin');
+    cssmin = require('gulp-cssmin');
 
 gulp.task('sass', function () {
     return gulp.src('app/scss/**/*.scss')
@@ -24,19 +23,25 @@ gulp.task('style', function () {
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/animate.css/animate.css',
+        // 'node_modules/rateyo/min/jquery.rateyo.min.css',
+        // 'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+        // 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
+        // 'node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css',
+        // 'node_modules/rateyo/min/jquery.rateyo.min.css',
+        // 'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
         .pipe(gulp.dest('app/css'))
 
 });
-gulp.task('img-compress', function () {
-    return gulp.src('./app/images/**')
-        .pipe(imagemin({
-            progressive: true
-        }))
-        .pipe(gulp.dest('./build/images/'))
-});
+// gulp.task('img-compress', function () {
+//     return gulp.src('./app/images/**')
+//         .pipe(imagemin({
+//             progressive: true
+//         }))
+//         .pipe(gulp.dest('./build/images/'))
+// });
 
 gulp.task('html', function () {
     return gulp.src('app/*.html')
@@ -47,6 +52,12 @@ gulp.task('script', function () {
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/wow.js/dist/wow.js',
+        // 'node_modules/rateyo/src/jquery.rateyo.js',
+        // 'node_modules/mixitup/dist/mixitup.js',
+        // 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+        // 'node_modules/jquery-form-styler/dist/jquery.formstyler.js'
+        // 'node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js',
+        
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
@@ -70,10 +81,10 @@ gulp.task('browser-sync', function () {
 
 
 gulp.task('watch', function () {
-    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
-    gulp.watch('app/*.html', gulp.parallel('html'))
-    gulp.watch('app/js/*.js', gulp.parallel('js'))
-    gulp.watch('app/images/**', gulp.parallel('img-compress'))
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
+    gulp.watch('app/*.html', gulp.parallel('html'));
+    gulp.watch('app/js/*.js', gulp.parallel('js'));
+    // gulp.watch('app/images/**', gulp.parallel('img-compress'))
 });
 
 gulp.task('default', gulp.parallel('style', 'sass', 'script', 'watch', 'browser-sync'));
